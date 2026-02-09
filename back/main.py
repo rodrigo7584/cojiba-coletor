6b23e1fd2e8a52cb41acc96ad53699f4b88ee2ae
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com.br>
2026-02-09 14:04:24 -0300

    att versao

--- back/main.py
+++ back/main.py
@@ -263,5 +263,5 @@ def gerar_arquivo_cotacao(cotacao_id: int):
 
 @app.get("/versao")
 def versao():
-    return {"versao": "1.0.0", "mensagem": "API atualizadas"}
+    return {"versao": "1.0.1", "mensagem": "API atualizadas"}
  
