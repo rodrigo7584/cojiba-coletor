f86639264aaa61ae7458b1c60ff9757fa8b932f7
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com.br>
2026-02-10 20:22:48 -0300

    portas abertas

--- back/main.py
+++ back/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # libera só seu frontend
+    allow_origins=["*"],  # libera só seu frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
